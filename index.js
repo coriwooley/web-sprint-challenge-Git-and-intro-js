90d7a63a622d7738798dac522559687e6453da00
Cori Wooley <wooleycori@gmail.com>
2022-01-14 03:29:56 -0800

    completed task 5

--- index.js
+++ index.js
@@ -269,10 +269,15 @@ Use removeArtist to do the following:
 
 For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function removeArtist(array, number) {
+  for (let i = 0; i < array.length; i++){
+    if (array[i].id === number){
+      array.splice(i, 1)
+    }
+  }
+  return array.length
 }
-
+console.log(removeArtist(artists, 0))
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use addArtist to do the following: 
